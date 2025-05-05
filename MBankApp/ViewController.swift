ffd811a11d9d78bd480f976dc5db0687be8a5db9
PosavacIvan <ivanposavac2@gmail.com>
2025-05-05 16:51:13 +0200

    Initial commit

---
+++ MBankApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MBankApp
+//
+//  Created by Ivan Posavac on 04.05.2025..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
---
+++ MBankAppTests/MBankAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MBankAppTests.swift
+//  MBankAppTests
+//
+//  Created by Ivan Posavac on 04.05.2025..
+//
+
+import Testing
+@testable import MBankApp
+
+struct MBankAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
